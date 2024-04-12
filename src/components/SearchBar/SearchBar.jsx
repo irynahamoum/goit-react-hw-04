dbfb17c62db71b637788f559a459d4a7facf182c
irynahamoum <111897707+irynahamoum@users.noreply.github.com>
2024-04-12 09:39:17 +0200

    add toast message on empty input search

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,14 +1,22 @@
 import style from './SearchBar.module.css';
 import { FcSearch } from 'react-icons/fc';
+import toast, { Toaster } from 'react-hot-toast';
 
 const SearchBar = ({ handleQuery }) => {
   function handleSubmit(e) {
     e.preventDefault();
-    const value = e.currentTarget.elements.searchInput.value;
+    const value = e.currentTarget.elements.searchInput.value.trim();
+    if (value === '') {
+      toast.error('The search field is empty! Please type the value for search.', { position: 'top-right' });
+      e.currentTarget.reset();
+      return;
+    }
     handleQuery(value);
   }
+
   return (
     <header className={style.header}>
+      <Toaster />
       <form className={style.form} onSubmit={handleSubmit}>
         <input
           className={style.input}
